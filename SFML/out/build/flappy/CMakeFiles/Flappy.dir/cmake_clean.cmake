015fdcb14a35b0074fa58d12c9c5ff6058d858c0
tonkaS <tonka.segvic@fer.hr>
2025-01-27 16:11:21 +0100

    update CMakeLists to work with mac and linux

---
+++ SFML/out/build/flappy/CMakeFiles/Flappy.dir/cmake_clean.cmake
@@ -0,0 +1,32 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Flappy.dir/link.d"
+  "CMakeFiles/Flappy.dir/Parameters.cpp.o"
+  "CMakeFiles/Flappy.dir/Parameters.cpp.o.d"
+  "CMakeFiles/Flappy.dir/SelectionScreen.cpp.o"
+  "CMakeFiles/Flappy.dir/SelectionScreen.cpp.o.d"
+  "CMakeFiles/Flappy.dir/Simulator.cpp.o"
+  "CMakeFiles/Flappy.dir/Simulator.cpp.o.d"
+  "CMakeFiles/Flappy.dir/cgp_andrija/CGP1.cpp.o"
+  "CMakeFiles/Flappy.dir/cgp_andrija/CGP1.cpp.o.d"
+  "CMakeFiles/Flappy.dir/cgp_andrija/CGP1Individual.cpp.o"
+  "CMakeFiles/Flappy.dir/cgp_andrija/CGP1Individual.cpp.o.d"
+  "CMakeFiles/Flappy.dir/gp_tonka/FunctionBinaryTree.cpp.o"
+  "CMakeFiles/Flappy.dir/gp_tonka/FunctionBinaryTree.cpp.o.d"
+  "CMakeFiles/Flappy.dir/gp_tonka/GA.cpp.o"
+  "CMakeFiles/Flappy.dir/gp_tonka/GA.cpp.o.d"
+  "CMakeFiles/Flappy.dir/gp_tonka/TreeParser.cpp.o"
+  "CMakeFiles/Flappy.dir/gp_tonka/TreeParser.cpp.o.d"
+  "CMakeFiles/Flappy.dir/main.cpp.o"
+  "CMakeFiles/Flappy.dir/main.cpp.o.d"
+  "CMakeFiles/Flappy.dir/nn/NNlogic.cpp.o"
+  "CMakeFiles/Flappy.dir/nn/NNlogic.cpp.o.d"
+  "CMakeFiles/Flappy.dir/nn/NeuralNetwork.cpp.o"
+  "CMakeFiles/Flappy.dir/nn/NeuralNetwork.cpp.o.d"
+  "Flappy"
+  "Flappy.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Flappy.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ SFML/out/build/flappy/CMakeFiles/Flappy.dir/link.d
@@ -0,0 +1,199 @@
+Flappy: \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/Scrt1.o \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/crti.o \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/crtbeginS.o \
+  CMakeFiles/Flappy.dir/cgp_andrija/CGP1.cpp.o \
+  CMakeFiles/Flappy.dir/cgp_andrija/CGP1Individual.cpp.o \
+  CMakeFiles/Flappy.dir/nn/NNlogic.cpp.o \
+  CMakeFiles/Flappy.dir/nn/NeuralNetwork.cpp.o \
+  CMakeFiles/Flappy.dir/Parameters.cpp.o \
+  CMakeFiles/Flappy.dir/SelectionScreen.cpp.o \
+  CMakeFiles/Flappy.dir/Simulator.cpp.o \
+  CMakeFiles/Flappy.dir/main.cpp.o \
+  CMakeFiles/Flappy.dir/gp_tonka/FunctionBinaryTree.cpp.o \
+  CMakeFiles/Flappy.dir/gp_tonka/GA.cpp.o \
+  CMakeFiles/Flappy.dir/gp_tonka/TreeParser.cpp.o \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/libsfml-graphics.so \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/libsfml-system.so \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/libsfml-window.so \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/libgomp.so \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/libstdc++.so \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/libm.so \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/libm.so \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/libm.so \
+  /usr/lib/libm.so.6 \
+  /usr/lib/libmvec.so.1 \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/libgcc_s.so \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/libgcc_s.so \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/libgcc_s.so \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/libgcc_s.so.1 \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/libgcc.a \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/libgcc.a \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/libc.so \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/libc.so \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/libc.so \
+  /usr/lib/libc.so.6 \
+  /usr/lib/libc_nonshared.a \
+  /usr/lib/ld-linux-x86-64.so.2 \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/libgcc_s.so \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/libgcc_s.so \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/libgcc_s.so \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/libgcc_s.so.1 \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/libgcc.a \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/libgcc.a \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/crtendS.o \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/crtn.o \
+  /usr/lib/libfreetype.so.6 \
+  /usr/lib/ld-linux-x86-64.so.2 \
+  /usr/lib/libX11.so.6 \
+  /usr/lib/libXrandr.so.2 \
+  /usr/lib/libXcursor.so.1 \
+  /usr/lib/libudev.so.1 \
+  /usr/lib/libz.so.1 \
+  /usr/lib/libbz2.so.1.0 \
+  /usr/lib/libpng16.so.16 \
+  /usr/lib/libharfbuzz.so.0 \
+  /usr/lib/libbrotlidec.so.1 \
+  /usr/lib/libxcb.so.1 \
+  /usr/lib/libXext.so.6 \
+  /usr/lib/libXrender.so.1 \
+  /usr/lib/libXfixes.so.3 \
+  /usr/lib/libcap.so.2 \
+  /usr/lib/libglib-2.0.so.0 \
+  /usr/lib/libgraphite2.so.3 \
+  /usr/lib/libbrotlicommon.so.1 \
+  /usr/lib/libXau.so.6 \
+  /usr/lib/libXdmcp.so.6 \
+  /usr/lib/libpcre2-8.so.0
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/Scrt1.o:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/crti.o:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/crtbeginS.o:
+
+CMakeFiles/Flappy.dir/cgp_andrija/CGP1.cpp.o:
+
+CMakeFiles/Flappy.dir/cgp_andrija/CGP1Individual.cpp.o:
+
+CMakeFiles/Flappy.dir/nn/NNlogic.cpp.o:
+
+CMakeFiles/Flappy.dir/nn/NeuralNetwork.cpp.o:
+
+CMakeFiles/Flappy.dir/Parameters.cpp.o:
+
+CMakeFiles/Flappy.dir/SelectionScreen.cpp.o:
+
+CMakeFiles/Flappy.dir/Simulator.cpp.o:
+
+CMakeFiles/Flappy.dir/main.cpp.o:
+
+CMakeFiles/Flappy.dir/gp_tonka/FunctionBinaryTree.cpp.o:
+
+CMakeFiles/Flappy.dir/gp_tonka/GA.cpp.o:
+
+CMakeFiles/Flappy.dir/gp_tonka/TreeParser.cpp.o:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/libsfml-graphics.so:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/libsfml-system.so:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/libsfml-window.so:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/libgomp.so:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/libstdc++.so:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/libm.so:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/libm.so:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/libm.so:
+
+/usr/lib/libm.so.6:
+
+/usr/lib/libmvec.so.1:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/libgcc_s.so.1:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/libgcc.a:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/libgcc.a:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/libc.so:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/libc.so:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/libc.so:
+
+/usr/lib/libc.so.6:
+
+/usr/lib/libc_nonshared.a:
+
+/usr/lib/ld-linux-x86-64.so.2:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/libgcc_s.so.1:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/libgcc.a:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/libgcc.a:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/crtendS.o:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/crtn.o:
+
+/usr/lib/libfreetype.so.6:
+
+/usr/lib/ld-linux-x86-64.so.2:
+
+/usr/lib/libX11.so.6:
+
+/usr/lib/libXrandr.so.2:
+
+/usr/lib/libXcursor.so.1:
+
+/usr/lib/libudev.so.1:
+
+/usr/lib/libz.so.1:
+
+/usr/lib/libbz2.so.1.0:
+
+/usr/lib/libpng16.so.16:
+
+/usr/lib/libharfbuzz.so.0:
+
+/usr/lib/libbrotlidec.so.1:
+
+/usr/lib/libxcb.so.1:
+
+/usr/lib/libXext.so.6:
+
+/usr/lib/libXrender.so.1:
+
+/usr/lib/libXfixes.so.3:
+
+/usr/lib/libcap.so.2:
+
+/usr/lib/libglib-2.0.so.0:
+
+/usr/lib/libgraphite2.so.3:
+
+/usr/lib/libbrotlicommon.so.1:
+
+/usr/lib/libXau.so.6:
+
+/usr/lib/libXdmcp.so.6:
+
+/usr/lib/libpcre2-8.so.0:
